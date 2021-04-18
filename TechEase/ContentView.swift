89874adc3761d7d485ddd5839c635f4d194d59bb
AricaLConrad <aricaconrad@outlook.com>
2021-04-17 18:57:18 -0700

    Initial Commit

---
+++ TechEase/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TechEase
+//
+//  Created by Student Account on 4/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TechEase/TechEaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechEaseApp.swift
+//  TechEase
+//
+//  Created by Student Account on 4/17/21.
+//
+
+import SwiftUI
+
+@main
+struct TechEaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
