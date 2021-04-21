89a31ae00d5afb2211b127117ab6368818fea783
AricaLConrad <aricaconrad@outlook.com>
2021-04-21 13:24:26 -0700

    Modified the design of the TechEaseTutorialList file to match our prototype (major button and text design changes).

--- TechEase/TechEaseTutorialList.swift
+++ TechEase/TechEaseTutorialList.swift
@@ -3,6 +3,7 @@
 //  TechEase
 //
 //  Created by Natalman Nahm on 4/20/21.
+//  Modified by Arica Conrad on 4/20/21.
 //
 
 import SwiftUI
@@ -16,40 +17,77 @@ struct Tutorial: Identifiable{
 struct viewTutorial: View {
     let tutorialDisplay: Tutorial
     
+    /*
+    Arica: I modified the design of the image and text within the button. Yes, there are a lot of Spacers, but that is how I think I was able to get the image to be on the far left and the text to be centered within the button.
+    */
+    
     var body: some View{
         Button(action:{ }) {
             HStack{
                 Image(systemName: tutorialDisplay.Icon)
                     .resizable()
-                    .frame(width:32, height: 32)
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 40, height: 40)
+                Spacer()
+                Spacer()
                 Text(tutorialDisplay.TutorialName)
-                    .padding(.leading, 28.0)
+                    .font(.title2)
+                    .foregroundColor(Color("Black"))
+                Spacer()
+                Spacer()
                 Spacer()
             }
-            .padding(.leading, 42.0)
         }
-        .padding([.top, .leading, .trailing], 28.0)
-            .buttonStyle(RoundedButton())
+        .padding(10)
+        .buttonStyle(RoundedButton())
     }
 }
 
 struct TechEaseTutorialList: View {
     
+    /*
+    Arica: I modified the "Apps" icon and the "Social Media" icons. I changed the "Apps" icon from "app.badge" to "square.grid.2x2.fill" to match our high-fidelity prototype more. I also modified the "Social Media" icon to be "person.2.fill" instead of "person.3". I found this new icon to be a better size and the icon being filled in makes it stand out more.
+    */
+    
     let TutorialList = [
         Tutorial(TutorialName: "Phone Features", Icon: "iphone"),
-        Tutorial(TutorialName: "Apps", Icon:"app.badge"),
+        Tutorial(TutorialName: "Apps", Icon:"square.grid.2x2.fill"),
         Tutorial(TutorialName: "Internet", Icon:"globe"),
-        Tutorial(TutorialName: "Social Media", Icon:"person.3"),
+        Tutorial(TutorialName: "Social Media", Icon:"person.2.fill"),
         
     ]
     
     
     var body: some View {
         VStack{
-            Text("Tap a button bellow to view the tutorials in that group.")
-                .multilineTextAlignment(.center)
-                .font(.system(size: 20.0))
-                .padding(.all, 24.0)
+            
+            /*
+             Arica: The title at the top will tell the user that this is the "Tutorials" screen, the screen that will display all the tutorial categories.
+             
+             IMPORTANT NOTE: THIS DESIGN IS PLACEHOLDER ONLY!
+            
+             This design will probably not be used when the app is further developed, as I imagine we will come up with a more sophisticated upper navigation menu. I will design that menu when it is implemented and will take this out at that time.
+             */
+            
+             Text("Tutorials")
+                .font(.largeTitle)
+                .fontWeight(.regular)
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                .foregroundColor(Color("Black"))
+                .padding()
+                .border(/*@START_MENU_TOKEN@*/Color("DarkGreen")/*@END_MENU_TOKEN@*/, width: 2)
+                .background(Color("LightGreen"))
+                .padding()
+            
+            /*
+            Arica: This is the instructional text placed at the top of the screen.
+            */
+            
+            Text("Tap a button below to view the tutorials in that group.")
+                .font(.title3)
+                .foregroundColor(Color("Black"))
+                .multilineTextAlignment(.leading)
+                .padding(10)
             
             List(TutorialList) { aTutorial in
                 viewTutorial(tutorialDisplay: aTutorial)
@@ -62,16 +100,15 @@ struct TechEaseTutorialList: View {
 struct RoundedButton: ButtonStyle {
     func makeBody(configuration: Self.Configuration) -> some View {
         return configuration
+            /*
+            Arica: I modified this section to make the buttons look like our high-fidelity prototype. That way, whenever RoundedButton is called, this styling will be used, keeping the design consistent.
+            */
             .label
-            .font(.system(size: 24.0))
-            .foregroundColor(.black)
             .padding()
-            .frame(maxWidth: .infinity, maxHeight: 72)
-            .background(Color(red: 177/255, green: 255/255, blue: 156/255))
-            .overlay(
-                RoundedRectangle(cornerRadius: 5) .stroke(Color.black, lineWidth: 3.5))
+            .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+            .background(RoundedRectangle(cornerRadius: 10).stroke(Color("DarkBlue"), lineWidth: 2).background(Color("LightBlue").cornerRadius(10)))
+            .foregroundColor(.black)
     }
-    
 }
 
 struct TechEaseTutorialList_Previews: PreviewProvider {
