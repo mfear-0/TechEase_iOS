9a5efc58a302b9b391669675963565d81641d1ec
AricaLConrad <aricaconrad@outlook.com>
2021-05-25 23:19:57 -0700

    I added a new folder that contains four new files that represent our four tutorial categories. Each file contains string arrays for the overview content for each tutorial. I also added MARK comments in the ListOfTutorials file for quick access to each array.

---
+++ TechEase/Tutorial Overview Content/PhoneFeaturesTutorialsContent.swift
@@ -0,0 +1,102 @@
+//
+//  PhoneFeaturesTutorialsContent.swift
+//  TechEase
+//
+//  Created by Arica Conrad on 5/25/21.
+//
+
+/*
+
+Arica: This file stores the arrays for each tutorial in the Phone Features category. Each array is a summary of one tutorial, and each string value in the array represents one bullet point of that tutorial's summary.
+ 
+    I have left MARK comments in this file to provide quick jump links to each tutorial.
+ 
+*/
+
+import Foundation
+
+// MARK: The Contact List tutorial.
+
+let contactListTutorialOverviewContent = [
+    "Opening the Phone app.",
+    "Navigating to the Contacts List tab.",
+    "Opening the Contacts app.",
+    "Adding a new contact.",
+    "Editing an existing contact.",
+    "Deleting a contact."
+]
+
+// MARK: The Calling tutorial.
+
+let callingTutorialOverviewContent = [
+    "Opening the Phone app.",
+    "Navigating to the Keypad tab.",
+    "Calling 911.",
+    "Calling someone on your contact list."
+]
+
+// MARK: The Voicemail tutorial.
+
+let voicemailTutorialOverviewContent = [
+    "Opening the Phone app.",
+    "Navigating to the Voicemail tab.",
+    "Listening to your voicemails.",
+    "Saving and deleting voicemails.",
+    "Setting up your voicemail."
+]
+
+// MARK: The Texting tutorial.
+
+let textingTutorialOverviewContent = [
+    "Opening the Messages app.",
+    "Creating and sending a text message.",
+    "Understanding the keyboard.",
+    "Replying to a text message.",
+    "Creating and replying to a group chat.",
+    "Sending images and videos through a text."
+]
+
+// MARK: The Camera tutorial.
+
+let cameraTutorialOverviewContent = [
+    "Opening the Camera app.",
+    "Taking a vertical photo.",
+    "Taking a horizontal photo.",
+    "Looking at the photo just taken.",
+    "Turning flash on and off.",
+    "Taking a panorama photo.",
+    "Other photo options."
+]
+
+// MARK: The Photos tutorial.
+
+let photosTutorialOverviewContent = [
+    "Opening the Photos app.",
+    "Viewing your photos and videos.",
+    "Looking at one specific photo.",
+    "Editing a photo.",
+    "Deleting a photo.",
+    "Sharing a photo through the Photos app."
+]
+
+// MARK: The Videos tutorial.
+
+let videosTutorialOverviewContent = [
+    "Opening the Camera app.",
+    "Recording a video.",
+    "Viewing the video just recorded.",
+    "Viewing videos in the Photos app.",
+    "Editing a video.",
+    "Deleting a video.",
+    "Sharing a video through the Photos app."
+]
+
+// MARK: The Settings tutorial.
+
+let settingsTutorialOverviewContent = [
+    "Opening the Settings app.",
+    "What the Settings app does.",
+    "Common areas to look at.",
+    "Customizing your background wallpaper.",
+    "Changing your passcode."
+]
---
+++ TechEase/Tutorial Overview Content/SocialMediaTutorialsContent.swift
@@ -0,0 +1,111 @@
+//
+//  SocialMediaTutorialsContent.swift
+//  TechEase
+//
+//  Created by Arica Conrad on 5/25/21.
+//
+
+/*
+
+Arica: This file stores the arrays for each tutorial in the Social Media category. Each array is a summary of one tutorial, and each string value in the array represents one bullet point of that tutorial's summary.
+ 
+    I have left MARK comments in this file to provide quick jump links to each tutorial.
+
+*/
+
+import Foundation
+
+// MARK: The Social Media Basics tutorial.
+
+let socialMediaBasicsTutorialOverviewContent = [
+    "What is social media?",
+    "Why do people use social media?"
+]
+
+// MARK: The Downloading Apps tutorial.
+
+let downloadingAppsTutorialOverviewContent = [
+    "Opening the App Store app.",
+    "Searching for social media apps.",
+    "Reading the terms and conditions.",
+    "Installing a social media app."
+]
+
+// MARK: The Creating Accounts tutorial.
+/*
+ 
+Arica: IMPORTANT NOTE! Because the Internet tutorials category also has a "Creating Accounts" tutorial, this tutorial is specified with "Social Media" in the middle of the array name. This is only for providing the arrays with different names. The lists of Internet and Social Media tutorials are not going to be altered to have different names.
+
+*/
+
+let creatingAccountsSocialMediaTutorialOverviewContent = [
+    "Why social media usually requires an account.",
+    "Common information you need to provide.",
+    "Creating a secure password."
+]
+
+// MARK: The Social Media Safety tutorial.
+
+let socialMediaSafetyTutorialOverviewContent = [
+    "Dos and don'ts of social media.",
+    "Scams and bots.",
+    "Implications of using social media."
+]
+
+// MARK: The Zoom tutorial.
+
+let zoomTutorialOverviewContent = [
+    "Downloading and opening the Zoom app.",
+    "Creating a Zoom account.",
+    "Joining and leaving a meeting.",
+    "Turning your camera on and off.",
+    "Turning your microphone on and off.",
+    "Creating a meeting.",
+    "Inviting others to join your meeting."
+]
+
+// MARK: The Facebook tutorial.
+
+let facebookTutorialOverviewContent = [
+    "Downloading and opening the Facebook app.",
+    "Creating a Facebook account.",
+    "Connecting with family and friends.",
+    "Creating a post.",
+    "Replying and reacting to posts.",
+    "Sharing posts.",
+    "Following others and others following you."
+]
+
+// MARK: The Twitter tutorial.
+
+let twitterTutorialOverviewContent = [
+    "Downloading and opening the Twitter app.",
+    "Creating a Twitter account.",
+    "Creating a post.",
+    "Replying and reacting to posts.",
+    "Sharing posts.",
+    "Following others and others following you."
+]
+
+// MARK: The Instagram tutorial.
+
+let instagramTutorialOverviewContent = [
+    "Downloading and opening the Instagram app.",
+    "Creating an Instagram account.",
+    "Creating a post.",
+    "Replying and reacting to posts.",
+    "Sharing posts.",
+    "Following others and others following you."
+]
+
+// MARK: The YouTube tutorial.
+
+let youtubeTutorialOverviewContent = [
+    "Downloading and opening the YouTube app.",
+    "Creating a YouTube account.",
+    "Searching for videos.",
+    "Watching a video.",
+    "Adding comments to a video.",
+    "Following others and others following you.",
+    "Creating a video."
+]
