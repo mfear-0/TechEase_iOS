f01ddf44e3b298e2708a84c7002c497b2ae45c49
mfear-0 <fearkenz@gmail.com>
2021-06-02 20:45:22 -0700

    initial attempt at file IO, not yet working. added FileIOTest file to project as well. -mfear

--- TechEase/ContentView.swift
+++ TechEase/ContentView.swift
@@ -6,61 +6,45 @@
 //  Hans Mandt, and Natalman Nahm on 4/17/21.
 //
 //
-//import SwiftUI
-//
-//struct ContentView: View {
-//    
-//    init() {
-//        UINavigationBar.appearance().backgroundColor = UIColor.green
-//    }
-//    var body: some View {
-//        VStack{
-//            
-//            NavigationView{
-//                
-//                VStack{
-//                    
-//                    Text("Tap a button below to view the tutorials in that group.")
-//                        .font(.title3)
-//                        .foregroundColor(Color("Black"))
-//                        .multilineTextAlignment(.leading)
-//                        .padding(10)
-//                    
-//                    List(tutorialList) { aTutorial in
-//                        viewTutorial(tutorialDisplay: aTutorial)
-//                            .padding()
-//                            .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
-//                            .background(RoundedRectangle(cornerRadius: 10).stroke(Color("DarkBlue"), lineWidth: 2).background(Color("LightBlue").cornerRadius(10)))
-//                            .foregroundColor(.black)
-//                    }
-//                    .padding(.top, 0)
-//                    .listStyle(PlainListStyle())
-//                    .navigationBarTitle("Tutorials", displayMode: .inline)
-//                    .navigationBarItems(leading:
-//                                            Button(action: {
-//                                                print("Button 1 pressed")
-//                                            }) {
-//                                                Image(systemName: "arrow.left").imageScale(.large)
-//                                            },
-//                                        trailing:
-//                                            Button(action: {}, label: {
-//                                                NavigationLink(destination: Homepage()) {
-//                                                    Text("Home")
-//                                                }
-//                                            }))
-//                }
-//            }
-//            
-//        }
-//    }
-//}
-//
-//
-//struct ContentView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        ContentView()
-//            .environmentObject(AppState())
-//    }
-//}
+import SwiftUI
+import Foundation
+
+struct ContentView: View {
+    
+    let url = Bundle.main.url(forResource: "FileIOTest", withExtension: "rtf")
+
+    
+    func printLine() -> String {
+        let filename = "FileIOTest.rtf"
+        var str1: String
+        var myCounter: Int
+        do {
+            let contents = try String(contentsOfFile: filename)
+            let lines = contents.split(separator:"\n")
+            myCounter = lines.count
+            str1 = String(myCounter)
+            } catch {
+                return (error.localizedDescription)
+            }
+            return str1
+    }
+
+
+    var body: some View {
+        VStack{
+            Text(printLine())
+            
+            
+        }
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(AppState())
+    }
+}
 
 
