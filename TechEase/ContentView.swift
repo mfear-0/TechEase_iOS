3eb66058379781c724d8cd47f7c352adc32365db
mfear-0 <fearkenz@gmail.com>
2021-06-06 15:33:55 -0700

    updated file IO and content screen. still working on content screen. -mfear

--- TechEase/ContentView.swift
+++ TechEase/ContentView.swift
@@ -11,28 +11,55 @@ import Foundation
 
 struct ContentView: View {
     
-    let url = Bundle.main.url(forResource: "FileIOTest", withExtension: "rtf")
+    func FileIO() -> [String] {
+        let str2 = ["default", "text"]
+        
 
-    
-    func printLine() -> String {
-        let filename = "FileIOTest.rtf"
-        var str1: String
-        var myCounter: Int
-        do {
-            let contents = try String(contentsOfFile: filename)
-            let lines = contents.split(separator:"\n")
-            myCounter = lines.count
-            str1 = String(myCounter)
+        if let filepath = Bundle.main.path(forResource: "The Compass Tutorial", ofType: "txt") {
+            do {
+
+                let contents = try String(contentsOfFile: filepath)
+                return contents.components(separatedBy: "\n")
+
             } catch {
-                return (error.localizedDescription)
+                print("contents not loaded")
+                return str2
             }
-            return str1
+        }
+        else {
+            print("not found")
+            return str2
+        }
+    }
+    
+    func textByLine(int1: Int) -> String {
+        
+        let textArray: [String] = FileIO()
+        let str1 = textArray[int1]
+        return str1
     }
 
 
     var body: some View {
         VStack{
-            Text(printLine())
+            Text(textByLine(int1: 0))
+                .font(.title)
+                .foregroundColor(Color("DarkBlue"))
+            Text(textByLine(int1: 1))
+                .foregroundColor(Color("Black"))
+                .font(.title3)
+                .multilineTextAlignment(.leading)
+                .padding()
+            Text(textByLine(int1: 2))
+                .foregroundColor(Color("Black"))
+                .font(.title3)
+                .multilineTextAlignment(.leading)
+                .padding()
+            Text(textByLine(int1: 3))
+                .foregroundColor(Color("Black"))
+                .font(.title3)
+                .multilineTextAlignment(.leading)
+                .padding()
             
             
         }
