c1682efb2475255f49b08c11f4d127a81a51a145
mfear-0 <fearkenz@gmail.com>
2021-10-18 12:00:54 -0700

    Finished practice tutorial view, added functionality for content view to move to practice tutorial view as well.

--- TechEase/TPTest.swift
+++ TechEase/TPTest.swift
@@ -8,42 +8,88 @@
 import SwiftUI
 
 struct TPTestView: View {
-    @State private var bgImg = Image("bg1")
+    @EnvironmentObject var appState: AppState
+    @State var action: Int?
     
-    @State private var xPos = 100.0
-    @State private var yPos = 100.0
+    @State private var bgImg = Image("phonebg")
+    @State private var xPos = 95.0
+    @State private var yPos = 265.0
+    @State private var bWidth = 100.0
+    @State private var bHeight = 100.0
+    @State private var bText = "Click Here"
+    @State private var edge1 = 10.0
+    @State private var inst = "Click the Compass app to open it."
+    @State private var cgColor: Color = Color.white
+    
     
     var body: some View {
+        
         VStack{
             //EmptyView()
             Button(action: {
                 switch self.bgImg {
-                case Image("bg1"):
-                    self.bgImg = Image("bg2")
+                case Image("phonebg"):
+                    self.bgImg = Image("compbg")
                     self.xPos = 200.0
-                    self.yPos = 200.0
-                case Image("bg2"):
-                    self.bgImg = Image("bg3")
-                    self.xPos = 150.0
-                    self.yPos = 150.0
+                    self.yPos = 667.0
+                    self.bWidth = 1000.0
+                    self.bText = " "
+                    self.edge1 = 300.0
+                    self.inst = "Click on coordinates to open the map."
+                    
+                case Image("compbg"):
+                    self.cgColor = Color.black
+                    self.bgImg = Image("mapbg")
+                    self.xPos = 195
+                    self.yPos = 393
+                    self.edge1 = 37.0
+                    self.inst = "This shows your location on the map. Click the blue marker to repeat the tutorial, or click here to return."
                 default:
-                    self.bgImg = Image("bg1")
-                    self.xPos = 100.0
-                    self.yPos = 100.0
+                    self.bgImg = Image("phonebg")
+                    self.xPos = 95.0
+                    self.yPos = 265.0
+                    self.edge1 = 10.0
+                    self.inst = "Click the Compass app to open it."
+                    self.bWidth = 100.0
+                    self.bHeight = 100.0
+                    self.bText = "Click Here"
+                    self.cgColor = Color.white
+
                 }
             }){
-                Text("Button Text")
+                Text(bText)
+                    .foregroundColor(.white)
                     .font(.system(size:24))
-                    .foregroundColor(.black)
+                    .padding(EdgeInsets(top: 10, leading: edge1, bottom: 10, trailing: 10))
+                    .border(cgColor, width: 5)
+                    
             }
-            .frame(width:100, height:100, alignment: .center)
+            .frame(width:bWidth, height:bHeight, alignment: .center)
             .padding(.all, 20)
             .position(x:CGFloat(xPos), y:CGFloat(yPos))
 
+            NavigationLink(destination: ContentView(), tag: 1, selection: $action){
+                Text(inst)
+                    .padding(EdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10))
+                    .foregroundColor(.white)
+                    .font(.system(size:22))
+                    .frame(alignment: .center)
+                    .background(cgColor.opacity(0.5))
+            }
+            .isDetailLink(false)
 
+
+            
+
+
+                
+            
+                
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(bgImg)
+        .background(bgImg
+                        .resizable()
+                        .aspectRatio(contentMode: .fill))
     }
     
 }
@@ -52,7 +98,7 @@ struct TPTestView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
             TPTestView()
-            TPTestView()
+                .environmentObject(AppState())
         }
     }
 }
